720a8e50e02cfe47833ec1d28dab852cb31a7b25
Filipe Laíns <lains@archlinux.org>
2020-05-28 02:11:36 +0100

    install: add build() and install() functions
    Signed-off-by: Filipe Laíns <lains@archlinux.org>

--- install/__init__.py
+++ install/__init__.py
@@ -1,6 +1,77 @@
 # SPDX-License-Identifier: MIT
 
+import compileall
+import logging
+import os
+import site
+import shutil
+import sys
+import sysconfig
+import zipfile
+
+from typing import List
+
+
 '''
 python-install - A simple, correct PEP427 wheel installer
 '''
 __version__ = '0.0.1'
+
+
+logger = logging.getLogger('install')
+
+
+class InstallException(Exception):
+    pass
+
+
+def _destdir_path(destdir, lib):  # type: (str, str) -> str
+    path = sysconfig.get_path(lib)
+    if not path:
+        raise InstallException("Couldn't find {}".format(lib))
+    return os.path.join(destdir, os.sep.join(path.split(os.sep)[1:]))
+
+
+def build(wheel, cache_dir, optimize):  # type: (str, str, List[int]) -> None
+    pkg_cache_dir = os.path.join(cache_dir, 'pkg')
+
+    try:
+        with zipfile.ZipFile(wheel) as wheel_zip:
+            wheel_zip.extractall(pkg_cache_dir)
+    except FileNotFoundError as e:
+        raise InstallException("File '{}' not found".format(e.filename))
+    except PermissionError as e:
+        raise InstallException("{}: '{}' ".format(e.strerror, e.filename))
+    except (ValueError, RuntimeError) as e:
+        raise InstallException(str(e))
+
+    for level in optimize:
+        logger.debug('Optimizing for {}'.format(level))
+        compileall.compile_dir(pkg_cache_dir, optimize=level)
+
+    # TODO: verify checksums
+    # TODO: generate entrypoint scripts
+
+
+def install(cache_dir, destdir, user=False):  # type: (str, str, bool) -> None  # noqa: C901
+    pkg_cache_dir = os.path.join(cache_dir, 'pkg')
+
+    def destdir_path(lib):  # type: (str) -> str
+        return _destdir_path(destdir, lib)
+
+    if user:
+        pkg_dir = site.getusersitepackages()
+    else:
+        # TODO: allow selecting one of the valid paths?
+        pkg_dir = site.getsitepackages()[0]
+
+    pkg_dir = os.path.join(destdir, os.sep.join(pkg_dir.split(os.sep)[1:]))
+
+    try:
+        if sys.version_info >= (3, 8):
+            shutil.copytree(pkg_cache_dir, pkg_dir, dirs_exist_ok=True)
+        else:
+            from distutils.dir_util import copy_tree
+            copy_tree(pkg_cache_dir, pkg_dir)
+    except FileExistsError as e:
+        raise InstallException("{}: '{}' ".format(e.strerror, e.filename))
--- install/__main__.py
+++ install/__main__.py
@@ -1,14 +1,15 @@
 # SPDX-License-Identifier: MIT
 
 import argparse
-import compileall
+import logging
 import os
 import shutil
-import site
 import sys
-import zipfile
 
-_VERBOSE = False
+from . import build, install, InstallException
+
+
+logger = logging.getLogger('install.main')
 
 
 def _error(msg, code=1):  # type: (str, int) -> None
@@ -17,15 +18,6 @@ def _error(msg, code=1):  # type: (str, int) -> None
         prefix = '\33[91m' + prefix + '\33[0m'
     print('{} {}'.format(prefix, msg))
     exit(code)
-
-
-def _verbose(msg):  # type: (str) -> None
-    if _VERBOSE:
-        prefix = suffix = ''
-        if sys.stdout.isatty():
-            prefix = '\33[2m'
-            suffix = '\33[0m'
-        print(prefix + msg + suffix)
 
 
 if __name__ == '__main__':  # noqa: C901
@@ -55,7 +47,8 @@ if __name__ == '__main__':  # noqa: C901
                         help='skip the cache building step, requires cache to be present already')
     args = parser.parse_args()
 
-    _VERBOSE = args.verbose
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
 
     cache_dir = '.install-cache'
     pkg_cache_dir = os.path.join(cache_dir, 'pkg')
@@ -70,7 +63,7 @@ if __name__ == '__main__':  # noqa: C901
     if os.path.exists(cache_dir):
         if os.path.isdir(cache_dir):
             if not args.skip_build:
-                _verbose('Cache directory exists, removing')
+                logger.debug('Cache directory exists, removing')
                 shutil.rmtree(cache_dir)
         else:
             _error("The cache path ('{}') exists and it's not a directory".format(cache_dir))
@@ -78,40 +71,16 @@ if __name__ == '__main__':  # noqa: C901
     # Build cache
     if not args.skip_build:
         try:
-            with zipfile.ZipFile(args.wheel) as wheel:
-                wheel.extractall(pkg_cache_dir)
-        except FileNotFoundError as e:
-            _error("File '{}' not found".format(e.filename))
-        except PermissionError as e:
-            _error("{}: '{}' ".format(e.strerror, e.filename))
-        except (ValueError, RuntimeError) as e:
+            build(args.wheel, cache_dir, args.optimize)
+        except InstallException as e:
             _error(str(e))
-
-        for level in args.optimize:
-            _verbose('Optimizing for {}'.format(level))
-            compileall.compile_dir(pkg_cache_dir, optimize=level)
-
-        # TODO: verify checksums
-        # TODO: generate entrypoint scripts
 
     # Install to destination
     if not args.cache:
         if not os.path.isdir(cache_dir):
             _error('Missing installation cache (hint: python -m install --cache [ ... ])')
 
-        if args.user:
-            pkg_dir = site.getusersitepackages()
-        else:
-            # TODO: allow selecting one of the valid paths?
-            pkg_dir = site.getsitepackages()[0]
-
-        pkg_dir = os.path.join(args.destdir, os.sep.join(pkg_dir.split(os.sep)[1:]))
-
         try:
-            if sys.version_info >= (3, 8):
-                shutil.copytree(pkg_cache_dir, pkg_dir, dirs_exist_ok=True)
-            else:
-                from distutils.dir_util import copy_tree
-                copy_tree(pkg_cache_dir, pkg_dir)
-        except FileExistsError as e:
-            _error("{}: '{}' ".format(e.strerror, e.filename))
+            install(cache_dir, args.destdir, args.user)
+        except InstallException as e:
+            _error(str(e))
